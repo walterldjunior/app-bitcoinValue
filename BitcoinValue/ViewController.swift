5d712280e2c274d024abd5e508d48b2f932c6104
Walter Junior <juniordeonisio@gmail.com>
2018-01-03 16:24:53 -0200

    Adicionando Dispatch e finalizando app

--- BitcoinValue/ViewController.swift
+++ BitcoinValue/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var precoBitcoin: UILabel!
+    @IBOutlet weak var botaoAtualizar: UIButton!
+    @IBAction func atualizarPreco(_ sender: Any) {
+        self.recuperarPrecoBitcoin()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.recuperarPrecoBitcoin()
+    }
+    
+    func formatarPreco(preco: NSNumber) -> String {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.locale = Locale(identifier: "pt_BR")
+        
+        if let precoFinal = nf.string(from: preco) {
+            return precoFinal
+        }
+        
+        return "0,00"
+        
+    }
+    
+    func recuperarPrecoBitcoin() {
+        
+        self.botaoAtualizar.setTitle("Atualizando...", for: .normal)
         
         if let url = URL(string: "https://blockchain.info/pt/ticker") {
-            
             // dataTask é uma tarefa que fica executando até conseguir um retorno, sucesso ou erro.
             let tarefa = URLSession.shared.dataTask(with: url, completionHandler: { (dados, requisicao, erro) in
                 if erro == nil {
@@ -26,8 +50,14 @@ class ViewController: UIViewController {
                             //options: [] -> significa que no JSON não será aplicado sem nenhum formato
                             if let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: Any] {
                                 if let brl = objetoJson["BRL"] as? [String: Any] {
-                                    if let precoBRL = brl["buy"] as? Float {
-                                        print(precoBRL)
+                                    if let precoBRL = brl["buy"] as? Double {
+                                        let precoFormatado = self.formatarPreco(preco: NSNumber(value: precoBRL))
+                                        
+                                        // Para atualizar componentes de interface dentro da clouser usando um thread
+                                        DispatchQueue.main.async(execute: {
+                                            self.precoBitcoin.text = "R$ " + precoFormatado
+                                            self.botaoAtualizar.setTitle("Atualizar", for: .normal)
+                                        })
                                     }
                                 }
                             }
@@ -43,5 +73,6 @@ class ViewController: UIViewController {
             tarefa.resume()
         }
     }
+    
 }
 
